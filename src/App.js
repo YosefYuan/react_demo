1ca3822c34efaf1a63b636765b5fc674f315fea2
Yosef Yuan <yuanruixiangjet@163.com>
2020-01-14 10:52:00 +0800

    feat: add router map in app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import SetStateDemo from "./component/SetStateDemo";
 import UseStateDemo from "./component/UseStateDemo";
 
 const App = () => {
   return (
     <Router>
-      <Route
-        key="home"
-        path="/SetStateDemo"
-        exact={true}
-        component={SetStateDemo}
-      />
-      <Route
-        key="home"
-        path="/UseStateDemo"
-        exact={true}
-        component={UseStateDemo}
-      />
+      <div>
+        <Link to="/SetStateDemo">SetStateDemo</Link>
+        <Link to="/UseStateDemo">UseStateDemo</Link>
+      </div>
+      <Switch>
+        <Route
+          key="home"
+          path="/SetStateDemo"
+          exact={true}
+          component={SetStateDemo}
+        />
+        <Route
+          key="home"
+          path="/UseStateDemo"
+          exact={true}
+          component={UseStateDemo}
+        />
+      </Switch>
     </Router>
   );
 };
