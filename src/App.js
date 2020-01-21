a89115ffc32c84409e1d783a26952545d95bdca5
Yosef Yuan <yuanruixiangjet@163.com>
2020-01-21 15:34:59 +0800

    feat: UseContext

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
 import SetStateDemo from "./component/SetStateDemo";
 import UseStateDemo from "./component/UseStateDemo";
-import WithDraggable from "./component/WithDraggable";
-import DragProps from "./component/DragProps";
-import UseDrag from "./component/UseDrag";
+import UseContext from "./component/UseContext";
+import WithDraggable from "./hoc/WithDraggable";
+import DragProps from "./hoc/DragProps";
+import UseDrag from "./hoc/UseDrag";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
       <div>
         <Link to="/SetStateDemo"> SetStateDemo </Link>
         <Link to="/UseStateDemo"> UseStateDemo </Link>
+        <Link to="/UseContext"> UseContext </Link>
         <Link to="/withDraggable">withDraggable</Link>
         <Link to="/DragProps">DragProps</Link>
         <Link to="/UseDrag">UseDrag</Link>
@@ -29,6 +31,12 @@ const App = () => {
           path="/UseStateDemo"
           exact={true}
           component={UseStateDemo}
+        />
+        <Route
+          key="UseContext"
+          path="/UseContext"
+          exact={true}
+          component={UseContext}
         />
         <Route
           key="WithDraggable"
